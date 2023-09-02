ca1c9ccb0d1fa65a5b8ace3939117642095f3dc7
Pernilla Hällgren <pernillahallgren@PerHal-MacBook-Pro.local>
2023-09-02 15:55:56 +0200

    add api request

---
+++ src/services/api.tsx
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+export interface UserData {
+    login: string
+    id: number
+    node_id: string
+    avatar_url: string
+    gravatar_id: string
+    url: string
+    html_url: string
+    followers_url: string
+    following_url: string
+    gists_url: string
+    starred_url: string
+    subscriptions_url: string
+    organizations_url: string
+    repos_url: string
+    events_url: string
+    received_events_url: string
+    type: string
+    site_admin: boolean
+    name: string
+    company: string
+    blog: string
+    location: string
+    email: string
+    hireable: any
+    bio: string
+    twitter_username: any
+    public_repos: number
+    public_gists: number
+    followers: number
+    following: number
+    created_at: string
+    updated_at: string
+}
+
+const githubAccessToken = process.env.REACT_APP_GITHUB_ACCESS_TOKEN;
+
+const getUserData = async (username: string) => {
+    try {
+        const response = await axios.get<UserData>(`https://api.github.com/users/${username}`, {
+            headers: {
+                'Authorization': `Bearer ${githubAccessToken}`,
+            },
+        });
+        console.log('RESPONSE===>>>', response.data);
+        return response.data;
+    } catch (error: any) {
+        console.log(error.message);
+        return error;
+    }
+};
+  
+export { getUserData };
